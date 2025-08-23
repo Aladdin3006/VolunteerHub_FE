a54f78d3ed2edb71ef86cf6d77b00aa56d8f77f2
NhatTheGOD <nhatyhnl1258@gmail.com>
2025-08-24 00:53:17 +0700

    fix rescur

--- vite.config.ts
+++ vite.config.ts
@@ -25,17 +25,17 @@ export default defineConfig({
         orientation: "portrait",
         icons: [
           {
-            src: "/icons/icon-192x192.png",
+            src: "/logo-remove-bg.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icons/icon-512x512.png",
+            src: "/logo-remove-bg.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/icons/icon-512x512.png",
+            src: "logo-remove-bg.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
